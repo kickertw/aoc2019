752f0c4a393cdad8aac952068dfd36e7450d40bc
twong540 <twong@reliaslearning.com>
2019-12-12 09:59:12 -0500

    day 7 getting closer

--- day7/day7.go
+++ day7/day7.go
@@ -67,42 +67,43 @@ func runProgram(inputs []int, phaseSetting int, ampOutput int) int {
 		switch opCode {
 		case 1:
 			inputs[outputIndex] = p1 + p2
-			fmt.Printf("%v + %v = %v (Stored @ %v)\n", p1, p2, inputs[outputIndex], outputIndex)
+			//fmt.Printf("%v + %v = %v (Stored @ %v)\n", p1, p2, inputs[outputIndex], outputIndex)
 		case 2:
 			inputs[outputIndex] = p1 * p2
-			fmt.Printf("%v * %v = %v (Stored @ %v)\n", p1, p2, inputs[outputIndex], outputIndex)
+			//fmt.Printf("%v * %v = %v (Stored @ %v)\n", p1, p2, inputs[outputIndex], outputIndex)
 		case 3:
 			if phaseIsSet {
 				inputs[inputs[i+1]] = ampOutput
 			} else {
 				inputs[inputs[i+1]] = phaseSetting
+				phaseIsSet = true
 			}
 
-			fmt.Printf("input %v (Stored @ %v)\n", inputs[inputs[i+1]], inputs[i+1])
+			//fmt.Printf("input %v (Stored @ %v)\n", inputs[inputs[i+1]], inputs[i+1])
 		case 4:
 			ampOutput = p1
-			fmt.Printf("Amp Output = %v\n", p1)
+			//fmt.Printf("Amp Output = %v\n", p1)
 		case 5, 6:
 			if (opCode == 5 && p1 != 0) || (opCode == 6 && p1 == 0) {
-				fmt.Print("Conditional met! - ")
+				//fmt.Print("Conditional met! - ")
 				jumpIndex = p2
 			} else {
-				fmt.Print("Conditional not met! - ")
+				//fmt.Print("Conditional not met! - ")
 				jumpIndex = i + 3
 			}
-			fmt.Printf("Jumping to index %v\n", jumpIndex)
+			//fmt.Printf("Jumping to index %v\n", jumpIndex)
 		case 7:
 			inputs[outputIndex] = 0
 			if p1 < p2 {
 				inputs[outputIndex] = 1
 			}
-			fmt.Printf("Setting %v @ index %v\n", inputs[outputIndex], outputIndex)
+			//fmt.Printf("Setting %v @ index %v\n", inputs[outputIndex], outputIndex)
 		case 8:
 			inputs[outputIndex] = 0
 			if p1 == p2 {
 				inputs[outputIndex] = 1
 			}
-			fmt.Printf("Setting %v @ index %v\n", inputs[outputIndex], outputIndex)
+			//fmt.Printf("Setting %v @ index %v\n", inputs[outputIndex], outputIndex)
 		}
 
 		// move the instruction pointer
@@ -114,21 +115,39 @@ func runProgram(inputs []int, phaseSetting int, ampOutput int) int {
 			i = jumpIndex
 		}
 	}
+
+	return ampOutput
+}
+
+func permutations(arr []int) [][]int {
+	var helper func([]int, int)
+	res := [][]int{}
+
+	helper = func(arr []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+	helper(arr, len(arr))
+	return res
 }
 
 func main() {
-
-	// Get User Input
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("-> ")
-	text, _ := reader.ReadString('\n')
-
-	userInput, err := strconv.Atoi(strings.TrimSuffix(text, "\r\n"))
-	if err != nil {
-		userInput = 5
-	}
-	fmt.Printf("Input = %v\n", userInput)
-
 	// Printing out file contents
 	inputfile := "day7input.txt"
 
@@ -144,10 +163,22 @@ func main() {
 
 	inputs := stringToIntArr(input)
 
-	ampInput := []int{4, 3, 2, 1, 0}
-	ampOutput := 0
-	for _, val := range ampInput {
-		ampOutput = runProgram(inputs, val, ampOutput)
+	winningCombo := []int{}
+	largestOutput := 0
+	ampInput := []int{0, 1, 2, 3, 4}
+	perms := permutations(ampInput)
+
+	for ii := 0; ii < len(perms); ii++ {
+		ampOutput := 0
+		for jj := 0; jj < len(perms[ii]); jj++ {
+			ampOutput = runProgram(inputs, perms[ii][jj], ampOutput)
+		}
+
+		if ampOutput > largestOutput {
+			largestOutput = ampOutput
+			winningCombo = perms[ii]
+		}
 	}
 
+	fmt.Printf("Largest Output = %v / Amp Seq = %v", largestOutput, winningCombo)
 }
